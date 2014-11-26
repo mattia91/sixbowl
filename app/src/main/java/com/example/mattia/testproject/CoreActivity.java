d350f567971ed799d6f4e43409f1a1d302a3eca7
mattia <mattiabenzoni@gmail.com>
2014-11-26 16:47:12 +0100

    Ho aggiunto qualcosa di grafica ma non funziona bene

--- app/src/main/java/com/example/mattia/testproject/CoreActivity.java
+++ app/src/main/java/com/example/mattia/testproject/CoreActivity.java
@@ -1,5 +1,8 @@
 package com.example.mattia.testproject;
 
+import android.view.View;
+import android.widget.Button;
+
 import com.example.mattia.testproject.Model.Board;
 import com.example.mattia.testproject.Model.Bowl;
 
@@ -9,6 +12,13 @@ import com.example.mattia.testproject.Model.Bowl;
 public class CoreActivity {
 
     Board board = new Board();
+
+    MainActivity mainActivity;
+    Bowl bowltemp=new Bowl();
+
+    public CoreActivity(MainActivity mainActivity) {
+        this.mainActivity=mainActivity;
+    } // costruttore
 
 
 
@@ -21,6 +31,51 @@ public class CoreActivity {
             board.setEnd_of_game( board.getGame_over().gameOver(board.getLast_active(), board.getPlayers()) );
         }
         return null;
+    }
+
+    // metodo che fa da interfaccia
+    // ricevo come parametro l' id (i) del bottone premuto e il bottone stesso ( view )
+    // dall' id prendo la bowl corrispondente
+
+    public Bowl interfaccia(int i, View view){  // questa view passata come parametro no è tutta la view ma solo il bottone cliccato
+
+        if((i>=0)&&(i<=5)){
+            bowltemp=board.getPlayers().get(0).getBowls().get(i);
+        }
+        else if((i>=8) && (i<=13)){
+
+            bowltemp = board.getPlayers().get(1).getBowls().get(i);
+        }
+
+        bowltemp = user_action(bowltemp);
+
+        updateView(view); // richiamo il metodo che dovrebbe aggiornare la view
+
+        return null;  // quando faccio la return la grafica non si aggiorna e l' appllicazione si blocca
+    }
+
+
+  // metodo che dovrebbe aggiornare la view
+  // con il primo ciclo scorro i primi sei bottoni e i due bottoni tray e modifico il Text del bottone
+    private void updateView(View view) {
+
+        for(int i=0;i<8;i++) {
+            Button b = (Button) mainActivity.findViewById(i);
+
+            if(i==6)
+                b.setText(Integer.toString(board.getPlayers().get(0).getTray().getSeeds()));
+            if (i==7)
+                b.setText(Integer.toString(board.getPlayers().get(1).getTray().getSeeds()));
+
+            b.setText(Integer.toString(board.getPlayers().get(0).getBowls().get(5-i).getNum_seeds()));
+        }
+
+      // aggiorno il Text degli altri sei
+        for(int i=8;i<14;i++){
+            int k=i;
+            Button b=(Button)mainActivity.findViewById(k);
+            b.setText(Integer.toString(board.getPlayers().get(1).getBowls().get(i-6-2).getNum_seeds()));
+        }
     }
 
 
--- app/src/main/java/com/example/mattia/testproject/MainActivity.java
+++ app/src/main/java/com/example/mattia/testproject/MainActivity.java
@@ -1,18 +1,103 @@
 package com.example.mattia.testproject;
 
+import android.view.View.OnClickListener;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+
+    private CoreActivity core = new CoreActivity(this); // ho dichiarato qusta nuova variabile
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        LinearLayout linLayout = new LinearLayout(this);
+        linLayout.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams linLayoutParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        setContentView(linLayout, linLayoutParam);
+
+        // -------------------------------------------------------------------------------------------------------------------------------------------
+        TableLayout tableLayout = new TableLayout(this);  // creo il layout della tabella
+
+        TableRow tableRow = new TableRow(this);  // creo la prima riga della tabella
+
+        int i = 0;
+        for (; i < 6; i++) {
+            Button button = new Button(this);  // creo il bottone che va nella prima cella dell aprima riga
+            button.setId(i);
+            button.setOnClickListener(this);
+            button.setText("3");
+            tableRow.addView(button);  // aggiungo il bottone alla riga
+        }
+        tableLayout.addView(tableRow); // aggiungo la riga alla tabella
+
+        // creo primo tray
+        tableRow = new TableRow(this);
+        Button button = new Button(this);
+        button.setEnabled(false);
+        i=5;
+        button.setId(i + 1);
+        button.setText("0");
+        tableRow.addView(button, 0);
+
+        // creo 4 bottoni non utilizzabili giusto per creare  spazio centrale nella seconda riga
+        for (int j = 1; j < 5; j++) {
+            button = new Button(this);
+            button.setVisibility(View.INVISIBLE);
+            tableRow.addView(button, j);
+        }
+
+        // creo secondo tray
+        button = new Button(this);
+        i=6;
+        button.setId(i + 2);
+        button.setEnabled(false);
+        button.setText("0");
+        tableRow.addView(button, 5);
+        tableLayout.addView(tableRow); // aggiungo la riga alla tabella
+
+        tableRow = new TableRow(this);
+        for (i = 0; i < 6; i++) {
+            button = new Button(this);  // creo il bottone che va nella prima cella dell aprima riga
+            button.setOnClickListener(this);
+            button.setId(i + 8);
+            button.setText("3");
+            tableRow.addView(button);  // aggiungo il bottone alla riga
+        }
+        tableLayout.addView(tableRow); // aggiungo la riga alla tabella
+
+        // -----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+        RelativeLayout.LayoutParams centerTableParameters = new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        centerTableParameters.addRule(RelativeLayout.CENTER_IN_PARENT);
+
+        linLayout.addView(tableLayout, centerTableParameters);
+        setContentView(linLayout);
+
     }
+
+   // metodo eseguito quando un bottone viene premuto
+    @Override
+    public void onClick(View view) {
+
+        core.interfaccia(view.getId(),view); // passa come parametri l' id del bottone e il bottone stesso
+
+    }
+
 
 
     @Override
@@ -36,4 +121,5 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+
+  }
