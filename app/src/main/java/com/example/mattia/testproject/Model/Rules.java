100c9079f962e1d4707cccbc112d5d6c1a73c899
martino <m.t.n@hotmail.it>
2014-11-26 18:57:56 +0100

    aggiornamento della classe rules

--- app/src/main/java/com/example/mattia/testproject/Model/Rules.java
+++ app/src/main/java/com/example/mattia/testproject/Model/Rules.java
@@ -33,49 +33,35 @@ public class Rules {
 //METODI PRIVATI DELLA CLASSE
 //DEFINIZIONE DELLE REGOLE, UNA PER UNA
     private Player keep_active(Bowl last_bowl, Player active, ArrayList<Player> players){
-        //cambio del turno, questa macchinazione con il ciclo for serve per generalizzare il numero di giocatori da 2 a quanti ne voglio
+        //cambio del turno
+        int active_pointer = players.indexOf(active);
         if( last_bowl != null ){
-            for( int i = 1 ; i < Constant.number_player ; i++ ){
-                if( players.get(i-1).equals(active) ){
-                    active = players.get(i);
-                    //salta quello dopo/ esce dal ciclo se imposto l'ultimo come attivo
-                    //i++;
-                    return active;
-                }
-                else if( i == Constant.number_player - 1 && players.get(i).equals(active)){
-                    active = players.get(0);
-                    return active;
-                }
+            if( active_pointer + 1 == Constant.number_player){
+                active_pointer = 0;
             }
+            else active_pointer++;
+            active = players.get(active_pointer);
         }
         //quanto segue è INUTILE, serve solo per capire cosa succede nel gioco, se ricevo last_bowl = null : mi trovo nel mio tray quindi resto io attivo
         //if( last_bowl == null){
             //il player active resta active
         //}
-
         return active;
     }
 
     private void steal_seeds(Bowl last_bowl, Player active, ArrayList<Player> players){
-
         if( last_bowl != null ){
             //controllo se il vaso finale del ciclo di semina è un vaso del player attivo ed era vuoto in partenza
             if( active.getBowls().contains(last_bowl) && last_bowl.getNum_seeds() == 1) {
                 //rubo tutti i semi dell'avversario nella bowl simmetrica
                 int bowls_pointer = active.getBowls().indexOf(last_bowl);
-
-                //capiamo chi è il diretto avversario, il giocatore subito alla destra di quello attivo (di fronte nel caso di 2 giocatori ma usando il for è estendibile)
-                Player opponent = active;
-                for (int i = 1; i < Constant.number_player; i++) {
-                    if (players.get(i - 1).equals(active)) {
-                        opponent = players.get(i);
-                        //salta quello dopo/ esce dal ciclo se imposto l'ultimo come opponent
-                        i++;
-                    }
-                    else if (i == Constant.number_player - 1 && players.get(i).equals(active)) {
-                        opponent = players.get(0);
-                    }
+                int opponent_pointer = players.indexOf(active) + 1;
+                //capiamo chi è il diretto avversario, il giocatore subito alla destra di quello attivo (di fronte nel caso di 2 giocatori)
+                Player opponent;
+                if( opponent_pointer == Constant.number_player){
+                    opponent_pointer = 0;
                 }
+                opponent = players.get(opponent_pointer);
                 //aggiorno il mio tray
                 active.increment_tray(opponent.getBowls().get(Constant.num_bowls - 1 - bowls_pointer).getNum_seeds());
                 active.increment_tray(1);
