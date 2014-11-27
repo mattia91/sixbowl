e4f8334b25353532cf22774205af8f9d0e4e19ac
martino <m.t.n@hotmail.it>
2014-11-27 11:08:19 +0100

    package view (solo struttura)

--- app/src/main/java/com/example/mattia/testproject/MainActivity.java
+++ app/src/main/java/com/example/mattia/testproject/MainActivity.java
@@ -17,8 +17,6 @@ import android.widget.TableRow;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-    private CoreActivity core = new CoreActivity(this); // ho dichiarato qusta nuova variabile
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,51 +32,6 @@ public class MainActivity extends Activity implements OnClickListener {
 
         TableRow tableRow = new TableRow(this);  // creo la prima riga della tabella
 
-        int i = 0;
-        for (; i < 6; i++) {
-            Button button = new Button(this);  // creo il bottone che va nella prima cella dell aprima riga
-            button.setId(5-i);
-            button.setOnClickListener(this);
-            button.setText("3");
-            tableRow.addView(button,i);  // aggiungo il bottone alla riga
-        }
-        tableLayout.addView(tableRow); // aggiungo la riga alla tabella
-
-        // creo primo tray
-        tableRow = new TableRow(this);
-        Button button = new Button(this);
-        button.setEnabled(false);
-        i=5;
-        button.setId(i + 1);
-        button.setText("0");
-        tableRow.addView(button, 0);
-
-        // creo 4 bottoni non utilizzabili giusto per creare  spazio centrale nella seconda riga
-        for (int j = 1; j < 5; j++) {
-            button = new Button(this);
-            button.setVisibility(View.INVISIBLE);
-            tableRow.addView(button, j);
-        }
-
-        // creo secondo tray
-        button = new Button(this);
-        i=6;
-        button.setId(i + 1);
-        button.setEnabled(false);
-        button.setText("0");
-        tableRow.addView(button, 5);
-        tableLayout.addView(tableRow); // aggiungo la riga alla tabella
-
-        tableRow = new TableRow(this);
-        for (i = 0; i < 6; i++) {
-            button = new Button(this);  // creo il bottone che va nella prima cella dell aprima riga
-            button.setOnClickListener(this);
-            button.setId(i+8);
-            button.setText("3");
-            tableRow.addView(button,i);  // aggiungo il bottone alla riga
-        }
-        tableLayout.addView(tableRow); // aggiungo la riga alla tabella
-
         // -----------------------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -93,8 +46,6 @@ public class MainActivity extends Activity implements OnClickListener {
    // metodo eseguito quando un bottone viene premuto
     @Override
     public void onClick(View view) {
-
-        core.interfaccia(view.getId(),view); // passa come parametri l' id del bottone e il bottone stesso
 
     }
 
---
+++ app/src/main/java/com/example/mattia/testproject/View/CoreActivity.java
@@ -0,0 +1,21 @@
+package com.example.mattia.testproject.View;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.mattia.testproject.MainActivity;
+import com.example.mattia.testproject.Model.Board;
+import com.example.mattia.testproject.Model.Bowl;
+
+/**
+ * Created by martino on 21/11/14.
+ */
+public class CoreActivity extends Activity{
+
+    Board board = new Board();
+
+
+
+
+}
